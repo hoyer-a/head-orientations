bace9e0dc1089d92a1225bb62ed81f5560d3774d
Anton Hoyer <156099087+hoyer-a@users.noreply.github.com>
2026-05-05 10:12:25 +0200

    update init

--- head_orientations/__init__.py
+++ head_orientations/__init__.py
@@ -16,7 +16,7 @@ from .head_orientation_class import HeadOrientationsDataset, HeadOrientations
 from .utils import find_orientation_directory
 from .dsp import far_field_correction, directional_transfer_function
 from .interpolate import interpolate
-from .metrics import barumerli_localization
+from .metrics import HeadOrientationsMetrics, barumerli_localization
 from .plot import (
     subplot_spectral_difference,
     plot_single_spectral_difference,
@@ -29,6 +29,7 @@ __all__ = [
     "far_field_correction",
     "directional_transfer_function",
     "interpolate",
+    "HeadOrientationsMetrics",
     "barumerli_localization",
     "subplot_spectral_difference",
     "plot_single_spectral_difference",
